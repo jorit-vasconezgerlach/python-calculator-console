04e655d6c7069e05a1c2f4cac4581e3831765767
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2021-09-07 16:20:19 +0200

    Update PY + endless typing

--- index.py
+++ index.py
@@ -1,14 +1,15 @@
-first = int(input())
-sign = input()
-second = int(input())
+while 0==0:
+    first = int(input())
+    sign = input()
+    second = int(input())
 
-if sign == "+":
-    final = first+second
-if sign == "/":
-    final = first/second
-if sign == "-":
-    final = first-second
-if sign == "*":
-    final = first*second
+    if sign == "+":
+        final = first+second
+    if sign == "/":
+        final = first/second
+    if sign == "-":
+        final = first-second
+    if sign == "*":
+        final = first*second
 
-print(final)
+    print(">>", final)
